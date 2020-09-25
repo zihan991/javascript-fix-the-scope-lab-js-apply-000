1836d066e16506004af043b23f237a64aac8df27
celestial-applet-1261 <zihan991@gmail.com>
2020-09-25 21:16:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
+  
     return "FUNKY!";
-  }
 }
+
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
